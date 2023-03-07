52c5ab205589dbf36f17d069dad6b4f987a4fa7e
Yeşim Gezici <gezici267@gmail.com>
2023-03-07 20:57:27 +0300

    ..

--- lib/calisanlar/calisan_ekle.dart
+++ lib/calisanlar/calisan_ekle.dart
@@ -1,4 +1,3 @@
-
 import 'package:fatura_app_2/constants.dart';
 import "package:flutter/material.dart";
 
@@ -17,196 +16,92 @@ class _CalisanEkleState extends State<CalisanEkle> {
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
         decoration: const BoxDecoration(
-           image: DecorationImage(
+          image: DecorationImage(
             image: AssetImage("assets/images/loggin2.png"),
             fit: BoxFit.cover,
           ),
         ),
-        child:SingleChildScrollView(
-            child: Column(
-              children: [
-                SizedBox(height: MediaQuery.of(context).size.height*0.15,),
-                Text(
-                  'Yeni Çalışan Ekle',
-                  style: TextStyle(
-                      color: kTextColor,
-                      fontSize: MediaQuery.of(context).size.width*0.07,
-                      fontWeight: FontWeight.bold,
-                      shadows: const [BoxShadow(color: Colors.black, offset: Offset(1,2),blurRadius: 3 ),],
-                  ),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.15,
+              ),
+              Text(
+                'Yeni Çalışan Ekle',
+                style: TextStyle(
+                  color: kTextColor,
+                  fontSize: MediaQuery.of(context).size.width * 0.07,
+                  fontWeight: FontWeight.bold,
+                  shadows: const [
+                    BoxShadow(color: Colors.black, offset: Offset(1, 2), blurRadius: 3),
+                  ],
                 ),
-                SizedBox(height: MediaQuery.of(context).size.height*0.1,),
-                Container(
-                  width: MediaQuery.of(context).size.width*0.8,
-                  child: TextFormField(
-                    style: const TextStyle(
-                            height: 1.5,
-                            ),
-                    decoration: const InputDecoration(
-                      contentPadding:
-                       EdgeInsets.only(left: 10, top: 10, bottom: 15),
-                       prefixIcon: Padding(
-                         padding: EdgeInsets.only(top: 17),
-                         child: Icon(
-                              Icons.person,
-                              color:kIconColor
-                                  ),
-                       ),
-                  enabledBorder: UnderlineInputBorder(      
-                      borderSide: BorderSide(color: kBorderColor),   
-                      ),  
-                   focusedBorder: UnderlineInputBorder(
-                    
-                      borderSide: BorderSide(color: kBorderColor) 
-                      ),
-                    label: Center(
-                    child: Text("Adı Soyadı"),
-                  ), 
-                  labelStyle: TextStyle(
-                   
-                    color: kTextColor
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.1,
+              ),
+              buildContainer(Icons.person, "Adı Soyadı"),
+              buildContainer(Icons.more_horiz, "TC Kimlik No"),
+              buildContainer(Icons.phone, "Telefon"),
+              buildContainer(Icons.email_outlined, "E-posta"),
+
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.055,
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 0.4,
+                  padding: EdgeInsets.all(
+                    MediaQuery.of(context).size.width * 0.02,
                   ),
-                  
-                    ),
-                  ),
-                  
-                ),
-               // SizedBox(height: MediaQuery.of(context).size.height*0.02,),
-                Container(
-                  width: MediaQuery.of(context).size.width*0.8,
-                  child: TextFormField(
-                    style: const TextStyle(
-                            height: 1.5,
-                            ),
-                    decoration: const InputDecoration(
-                      contentPadding:
-                      EdgeInsets.only(left: 10, top: 10, bottom: 15),
-                       prefixIcon: Padding(
-                         padding: EdgeInsets.only(top: 17),
-                         child: Icon(
-                              Icons.more_horiz,
-                              color: kIconColor,
-                                  ),
-                       ),
-                  enabledBorder: UnderlineInputBorder(      
-                      borderSide: BorderSide(color: kBorderColor),   
-                      ),  
-                   focusedBorder: UnderlineInputBorder(
-                    
-                      borderSide: BorderSide(color: kBorderColor) 
-                      ),
-                    label: Center(
-                    child: Text("TC Kimlik No"),
-                  ), 
-                  labelStyle: TextStyle(
-                   
-                    color: kTextColor
-                  ),
-                  
-                    ),
-                  ),
-                  
-                ),
-               //  SizedBox(height: MediaQuery.of(context).size.height*0.02,),
-                Container(
-                  width: MediaQuery.of(context).size.width*0.8,
-                  child: TextFormField(
-                    style: const TextStyle(
-                            height: 1.5,
-                            ),
-                    decoration: const InputDecoration(
-                      contentPadding:
-                      EdgeInsets.only(left: 10, top: 10, bottom: 15),
-                       prefixIcon: Padding(
-                         padding: EdgeInsets.only(top: 17),
-                         child: Icon(
-                              Icons.phone,
-                              color: kIconColor,
-                                  ),
-                       ),
-                  enabledBorder: UnderlineInputBorder(      
-                      borderSide: BorderSide(color: kBorderColor),   
-                      ),  
-                   focusedBorder: UnderlineInputBorder(
-                    
-                      borderSide: BorderSide(color: kBorderColor) 
-                      ),
-                    label:  Center(
-                    child: Text("Telefon"),
-                  ), 
-                  labelStyle: TextStyle(
-                   
-                    color: kTextColor
-                  ),
-                  
-                    ),
-                  ),
-                  
-                ),
-               //  SizedBox(height: MediaQuery.of(context).size.height*0.02,),
-                Container(
-                  width: MediaQuery.of(context).size.width*0.8,
-                  child: TextFormField(
-                    style: const TextStyle(
-                            height: 1.5,
-                            ),
-                    decoration: const InputDecoration(
-                      contentPadding:
-                      EdgeInsets.only(left: 10, top: 10, bottom: 15),
-                       prefixIcon: Padding(
-                         padding:  EdgeInsets.only(top: 17),
-                         child: Icon(
-                              Icons.email_outlined,
-                              color: kIconColor
-                                  ),
-                       ),
-                  enabledBorder: UnderlineInputBorder(      
-                      borderSide: BorderSide(color: kBorderColor),   
-                      ),  
-                   focusedBorder: UnderlineInputBorder(
-                    
-                      borderSide: BorderSide(color: kBorderColor) 
-                      ),
-                    label:  Center(
-                    child: Text("E-posta"),
-                  ), 
-                  labelStyle: TextStyle(
-                   
-                    color: kTextColor
-                  ),
-                  
-                    ),
-                  ),
-                  
-                ),
-                
-                SizedBox(height: MediaQuery.of(context).size.height*0.055,),
-                GestureDetector(
-                  onTap: () {
-                   
-                  },
-                  child: Container(
-                    width: MediaQuery.of(context).size.width*0.4,
-                    padding:  EdgeInsets.all(MediaQuery.of(context).size.width*0.02,),
-                    decoration: const BoxDecoration(
-                      color: kButtonColor,
-                      borderRadius: BorderRadius.all(Radius.circular(20)),
-                      boxShadow: [BoxShadow(color: Colors.black26,
+                  decoration:
+                      const BoxDecoration(color: kButtonColor, borderRadius: BorderRadius.all(Radius.circular(20)), boxShadow: [
+                    BoxShadow(
+                      color: Colors.black26,
                       offset: Offset(3, 3),
                       blurRadius: 5,
-                      )]
+                    )
+                  ]),
+                  child: Center(
+                    child: Text(
+                      'Ekle',
+                      style: TextStyle(color: kTextColor, fontSize: MediaQuery.of(context).size.width * 0.04),
                     ),
-                    child: Center(
-                      child: Text('Ekle',
-                      style: TextStyle(color: kTextColor,
-                      fontSize: MediaQuery.of(context).size.width*0.04),),),
                   ),
                 ),
-                
-              ],
-            ),
+              ),
+            ],
           ),
         ),
+      ),
+    );
+  }
+
+  Container buildContainer(IconData icon, String text) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.8,
+      child: TextFormField(
+        style: const TextStyle(
+          height: 1.5,
+        ),
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.only(left: 10, top: 10, bottom: 15),
+          prefixIcon: Padding(
+            padding: EdgeInsets.only(top: 17),
+            child: Icon(icon, color: kIconColor),
+          ),
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: kBorderColor),
+          ),
+          focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: kBorderColor)),
+          label: Center(
+            child: Text(text),
+          ),
+          labelStyle: TextStyle(color: kTextColor),
+        ),
+      ),
     );
   }
 }
